1d6489fa7cf579a463e270348ae898d92a5aac5b
TexTrue <3140846162@qq.com>
2023-09-01 10:18:11 +0800

    Re-linkage with Splasher

--- forge/src/main/java/net/krlite/bounced/forge/BouncedClientForge.java
+++ forge/src/main/java/net/krlite/bounced/forge/BouncedClientForge.java
@@ -1,7 +1,12 @@
 package net.krlite.bounced.forge;
 
+import dev.architectury.event.EventResult;
+import dev.architectury.event.events.client.ClientGuiEvent;
+import dev.architectury.event.events.client.ClientScreenInputEvent;
 import dev.architectury.platform.forge.EventBuses;
 import net.krlite.bounced.Bounced;
+import net.krlite.splasher.Splasher;
+import net.minecraft.client.gui.screen.TitleScreen;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.IExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
@@ -21,26 +26,24 @@ public class BouncedClientForge {
     }
 
     public void onInitializeClient(FMLClientSetupEvent event) {
-        /* TODO: Waiting for Splasher 4.0.6 released!
         ClientGuiEvent.INIT_POST.register((screen, screenAccess) -> {
             if (screen instanceof TitleScreen) {
                 ClientScreenInputEvent.MOUSE_CLICKED_POST.register((client, currentScreen, mouseX, mouseY, button) -> {
-                            double scaledWidth = screenAccess.getScreen().width;
-                            double centerX = scaledWidth / 2.0, y = 30, width = 310, height = 44;
-                            if (!Bounced.isIntro()
+                    double scaledWidth = screenAccess.getScreen().width;
+                    double centerX = scaledWidth / 2.0, y = 30, width = 310, height = 44;
+                    if (!Bounced.isIntro()
                                     && mouseX >= centerX - width / 2 && mouseX <= centerX + width / 2
                                     && mouseY >= y && mouseY <= y + height
-                            ) {
+                    ) {
 
-								if (!Bounced.isSplasherLoaded || !Splasher.isMouseHovering(scaledWidth, mouseX, mouseY)) {
-									// Linkage with Splasher
-                                    Bounced.push();
-								}
-                            }
-                            return EventResult.pass();
-                        });
+                        if (!Bounced.isSplasherLoaded || !Splasher.isMouseHovering(scaledWidth, mouseX, mouseY)) {
+                            // Linkage with Splasher
+                            Bounced.push();
+                        }
+                    }
+                    return EventResult.pass();
+                });
             }
         });
-         */
     }
 }
