108672a90abe5e783a8e4bf87521e45b7f03f01f
TexTrue <65154269+TexBlock@users.noreply.github.com>
2024-04-03 22:25:54 +0800

    don't use kts

--- settings.gradle.kts
+++
@@ -1,9 +0,0 @@
-pluginManagement {
-    repositories {
-        maven { url = uri("https://maven.fabricmc.net/") }
-        maven { url = uri("https://maven.architectury.dev/") }
-        maven { url = uri("https://maven.minecraftforge.net/") }
-        mavenCentral()
-        gradlePluginPortal()
-    }
-}
