34af1b57d970557656691fa964e34da558a7ac74
KrLite <68179735+KrLite@users.noreply.github.com>
2024-04-04 19:03:23 +0800

    fix

--- src/main/java/band/kessokuteatime/bounced/mixin/SplashTextAnimator.java
+++ src/main/java/band/kessokuteatime/bounced/mixin/SplashTextAnimator.java
@@ -32,7 +32,7 @@ class AccessibilityOnboardingTrigger {
 	 * Triggers and restarts the animation.
 	 */
 	@ModifyArg(
-			method = "renderBackground",
+			method = "render",
 			at = @At(
 					value = "INVOKE",
 					target = "Lnet/minecraft/client/gui/RotatingCubeMapRenderer;render(FF)V"
