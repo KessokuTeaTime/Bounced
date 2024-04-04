9164a57b54b0694d6a8084032fa6dc7cf6777a28
KrLite <68179735+KrLite@users.noreply.github.com>
2024-04-04 21:07:20 +0800

    fix jars

--- build.gradle.kts
+++ build.gradle.kts
@@ -84,12 +84,12 @@ publisher {
 
 	versionType.set("release")
 	projectVersion.set(project.version.toString())
-	gameVersions.set(listOf("1.20.4"))
+	gameVersions.set(listOf("1.20.3", "1.20.4"))
 	loaders.set(listOf("fabric", "quilt"))
 	
 	displayName.set("${display.name} ${libs.versions.mod.get()} for ${display.loader} ${display.version}")
 
-	artifact.set(tasks.jar)
+	artifact.set(tasks.remapJar)
 	addAdditionalFile(tasks.remapSourcesJar)
 
 	changelog.set(file("CHANGELOG.md"))
