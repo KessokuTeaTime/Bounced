422cf20d1231b7530a4f8be18240ace27fb45e19
KrLite <68179735+KrLite@users.noreply.github.com>
2024-04-05 18:25:50 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -86,6 +86,14 @@ publisher {
 	projectVersion.set(project.version.toString())
 	gameVersions.set(listOf("1.20.3", "1.20.4"))
 	loaders.set(listOf("fabric", "quilt"))
+
+	modrinthDepends.required("fabric-api")
+	modrinthDepends.optional("splasher")
+	modrinthDepends.embedded()
+
+	curseDepends.required("fabric-api")
+	curseDepends.optional("splasher")
+	curseDepends.embedded()
 	
 	displayName.set("${display.name} ${libs.versions.mod.get()} for ${display.loader} ${display.version}")
 
