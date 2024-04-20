6e6857c1d7f3187cf477eb408557b8ef90610ac5
KrLite <68179735+KrLite@users.noreply.github.com>
2024-04-21 07:25:58 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -86,6 +86,7 @@ publisher {
 	projectVersion.set(project.version.toString())
 	gameVersions.set(listOf("1.20.3", "1.20.4"))
 	loaders.set(listOf("fabric", "quilt"))
+	curseEnvironment.set("client")
 
 	modrinthDepends.required("fabric-api")
 	modrinthDepends.optional("splasher")
