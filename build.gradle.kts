096421a19629b6542b28a428f8762c9a74ff6016
KrLite <68179735+KrLite@users.noreply.github.com>
2024-07-18 06:36:29 +0800

    Update splasher & display

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,3 @@
-class Display {
-	lateinit var name: String
-	lateinit var loader: String
-	lateinit var version: String
-}
-
-var display: Display = Display()
-
 plugins {
 	base
 	java
@@ -15,12 +7,10 @@ plugins {
 	alias(libs.plugins.modpublisher)
 }
 
+val display = libs.versions.display
+
 group = libs.versions.maven.group.get()
 version = "${libs.versions.mod.get()}-${libs.versions.loader.get()}.${libs.versions.minecraft.get()}"
-
-display.name = libs.versions.display.name.get()
-display.loader = libs.versions.display.loader.get()
-display.version = libs.versions.display.version.get()
 
 base {
 	archivesName.set(libs.versions.archives.name)
